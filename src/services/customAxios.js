5e2002d851bebbe147c6c342d4cf16f048dffb01
Billy <longnd1@bravebits.vn>
2024-02-13 23:27:20 +0700

    Update customAxios

--- src/services/customAxios.js
+++ src/services/customAxios.js
@@ -4,11 +4,15 @@ import { KEY_INFO_USER } from "@utils/constant";
 import { infoUserSubscription } from "@utils/globalStates/initGlobalState";
 import { passLocalStorage } from "@utils/passLocalStorage";
 
-export const baseURL = import.meta.env.VITE_BACKEND_URL;
+// Base URL for API requests
+const baseURL = import.meta.env.VITE_BACKEND_URL;
+// Header to indicate no retry for failed requests
 const NO_RETRY_HEADER = "x-no-retry";
-
+// Check if the API is local
 const isLocalApi =
   baseURL?.includes("127.0.0.1") || baseURL?.includes("localhost");
+// Create an instance of Axios with baseURL
+const instance = axios.create({ baseURL: baseURL + "v1/api/" });
 
 const handleDelayApiLocal = async () => {
   if (isLocalApi) {
@@ -16,37 +20,30 @@ const handleDelayApiLocal = async () => {
   }
 };
 
-let instance = axios.create({
-  baseURL: baseURL + "v1/api/",
-});
-
-// Function to get the access token from passLocalStorage
-export const getInfoUserLocal = () => {
+// Function to get user info from local storage
+const getInfoUserLocal = () => {
   const infoUser = passLocalStorage.getItem(KEY_INFO_USER) || {};
   infoUserSubscription.state = infoUser;
   return infoUser;
 };
 
-// Handle refresh Token
+// Function to handle refreshing access token
 const handleRefreshToken = async () => {
   const refreshLocal = passLocalStorage?.getItem(KEY_INFO_USER)?.refreshToken;
   const res = await instance.post("/auth/refresh", { refreshLocal });
-  if (res && res.data) {
-    return res.data;
-  } else return;
+  return res?.data;
 };
 
-// Function to set the access token in the header
+// Function to set access token in request headers
 const setAccessToken = () => {
   const accessToken = getInfoUserLocal()?.accessToken;
-  if (accessToken) {
+  if (accessToken)
     instance.defaults.headers.common = {
       Authorization: `Bearer ${accessToken}`,
     };
-  }
 };
 
-// Add a request interceptor
+// Interceptor to add access token to request headers
 instance.interceptors.request.use(
   async function (config) {
     setAccessToken();
@@ -57,18 +54,15 @@ instance.interceptors.request.use(
   }
 );
 
-// Add a response interceptor
+// Interceptor to handle response
 instance.interceptors.response.use(
   async function (response) {
-    setAccessToken();
-
     await handleDelayApiLocal();
 
-    return response && response.data ? response.data : response;
+    return response?.data;
   },
-
   async function (error) {
-    setAccessToken();
+    // Handle token refresh for unauthorized errors
     if (
       error.config &&
       error.response &&
@@ -76,31 +70,31 @@ instance.interceptors.response.use(
       !error.config.headers[NO_RETRY_HEADER]
     ) {
       const data = await handleRefreshToken();
-      error.config.headers[NO_RETRY_HEADER] = "true";
-
+      error.config.headers[NO_RETRY_HEADER] = "true"; // Mark request to avoid retry
       if (data && data.accessToken && data.refreshToken) {
+        // Update access token and refresh token in local storage
         error.config.headers["Authorization"] = `Bearer ${data.accessToken}`;
-
         let infoUser = passLocalStorage.getItem(KEY_INFO_USER);
-
         infoUser = {
           ...infoUser,
           accessToken: data.accessToken,
           refreshToken: data.refreshToken,
         };
-
         passLocalStorage.setItem(KEY_INFO_USER, infoUser);
         return instance.request(error.config);
-      } else return;
+      }
     }
+
+    // Handle removal of user info from local storage for certain errors
     if (
       error.config &&
       error.response &&
       +error.response.status === 400 &&
       error.config.url === "/auth/refresh"
     ) {
+      // Remove user info from local storage
       passLocalStorage.removeItem(KEY_INFO_USER);
-      // window.location.href = "/login";
+      window.location.href = "/login";
     }
 
     await handleDelayApiLocal();
@@ -109,4 +103,4 @@ instance.interceptors.response.use(
   }
 );
 
-export default instance;
+export default instance; // Export the Axios instance
